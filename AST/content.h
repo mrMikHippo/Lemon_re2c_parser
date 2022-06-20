efb9491581ce3c5bf1783239ac9a8b14dba6939d
mrMikHippo <example@example.com>
2022-06-20 12:35:34 +0300

    Implemented CallOrdered and CallNamed expressions in round brackets, Temporarily removed StatementDefinition rules

--- AST/content.h
+++ AST/content.h
@@ -4,12 +4,11 @@
 #include "expression.h"
 #include <vector>
 
-
 class OneParamContent : public Token
 {
 public:
     using Elements = std::vector<Expression*>;
-    OneParamContent();
+    OneParamContent(Expression* expr);
 
     void addElement(Expression* el_);
     const Elements& getElements() const;
@@ -24,11 +23,24 @@ class TwoParamContent : public Token
 public:
     using ElementPair = std::pair<Token*, Expression*>;
     using Elements = std::vector<ElementPair>;
-    TwoParamContent();
+    TwoParamContent(std::pair<Token*, Expression*> el_);
 
     void addElement(ElementPair el_);
     const Elements& getElements() const;
 
 public:
     Elements elements;
+};
+
+template<typename T>
+class ParamsContent : public Token
+{
+public:
+    ParamsContent() {};
+
+    void addElement(T el_) {
+        elements.push_back(el_);
+    }
+private:
+    std::vector<T> elements;
 };
